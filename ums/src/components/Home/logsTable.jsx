a4e64486f6304b283a6d5977d270051c9bc677cf
haezelrosecarrera <carrerahaezel@gmail.com>
2023-11-25 07:11:01 +0800

    Cleaned Code

--- ums/src/components/Home/logsTable.jsx
+++ ums/src/components/Home/logsTable.jsx
@@ -1,7 +1,7 @@
 // utils
 import "bootstrap/dist/css/bootstrap.min.css";
-import PropTypes from "prop-types";
-import React, { useEffect, useState } from "react";
+// import PropTypes from "prop-types";
+import React, { useState } from "react";
 //data
 import LogData from "../Data/logs.json";
 
@@ -12,11 +12,10 @@ import SortControl from "../controls/sortControl";
 import TableControl from "../controls/tableControl";
 
 const LogsTable = ({
-    name
 }) => {
     // states
     const [logs, setLogs] = useState(LogData);
-    const [searchedLogs, setSearchedLogs] = useState(LogData);
+    const [processedLogs, setProcessedLogs] = useState(LogData);
     const [moved, setMoved] = useState(true);
 
     // const rerender = React.useReducer(() => ({}), {})[1]
@@ -28,14 +27,11 @@ const LogsTable = ({
         setLogs(data);
     }
 
-    const sortData = (data) => {
+    const processData = (data) => {
         setMoved(!moved);
-        const last = 1 * 5;
-        const first = last - 5;
         // rerender();
-        
-        setLogs(data.slice(first, last));
-        setSearchedLogs(data);
+        setLogs(data.slice(0, 5));
+        setProcessedLogs(data);
     }
 
     // useEffect(() => {
@@ -48,12 +44,12 @@ return (
         <div className="wrapper">
             <h2>Logs</h2>
             <div className="d-flex justify-content-end align-items-center mb-2">
-                <SortControl name={"sort-by"} items={searchedLogs} onSuccess={sortData}/>
-                <SearchControl name="`search-name" items={LogData} onSuccess={sortData}/>
+                <SortControl name={"sort-by"} items={processedLogs} onSuccess={processData}/>
+                <SearchControl name="`search-name" items={LogData} onSuccess={processData}/>
             </div>
             
-            <TableControl headers={headers} items={logs} allItems={LogData} onSuccess={sortData}/>
-            <PaginationControl items={searchedLogs} onSuccess={reloadLogs} moved={moved}/>
+            <TableControl headers={headers} items={logs} allItems={LogData} onSuccess={processData}/>
+            <PaginationControl items={processedLogs} onSuccess={reloadLogs} moved={moved}/>
         </div>
     </div>
     // </div>
@@ -61,6 +57,6 @@ return (
 
 export default LogsTable;
 
-LogsTable.propTypes = {
-    name: PropTypes.string
-}
+// LogsTable.propTypes = {
+
+// }
