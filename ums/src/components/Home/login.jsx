fd0fe8dc8f80ea6118f710800b2bca66447f86a9
John Mark G. Cueno <johnmarkg.cueno@jhnmrkgbrlcn.local>
2024-01-07 19:10:12 +0800

    test

--- ums/src/components/Home/login.jsx
+++ ums/src/components/Home/login.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import logo from "../../asset/logo.png";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Data from "./Data.json";
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -12,25 +11,29 @@ function Login() {
 
   const users = [
     { username: "cueno@gmail.com", password: "password" },
+    { username: "maceda@gmail.com", password: "pass123" },
   ];
 
   const navigate = useNavigate();
   
   const handleLogin = () => {
-  const matchedUser = users.find(
-    (user) => user.username === username && user.password === password);
-
+    const matchedUser = users.find(
+      (user) => user.username === username && user.password === password
+    );
+  
     if (matchedUser) {
       setLoggedIn(true);
       navigate("/dashboard");
-    } else {
-      setErrorMessage("Wrong username or password. Please try again.");
+    } else if (password) {
+      setErrorMessage("Wrong password. Please try again.");
+    } else if (username) {
+      setErrorMessage("Wrong email. Please try again.");
     }
-
   };
+  
   return (
     <>
-      <section className="vh-100 gradient-custom">
+      <section className="vh-100 gradient-custom" data-testid="login-component">
         <div className="container h-20">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col-12 col-md-8 col-lg-6 col-xl-5">
@@ -45,7 +48,7 @@ function Login() {
                     </h3>
 
                     <div className="form-outline text-start form-white mb-4">
-                      <label className="form-label " for="typeEmailX">
+                      <label className="form-label " htmlFor="typeEmailX">
                         Email Address
                       </label>
                       <input
@@ -58,7 +61,7 @@ function Login() {
                     </div>
 
                     <div className="form-outline text-start  form-white mb-4">
-                      <label className="form-label" for="typePasswordX">
+                      <label className="form-label" htmlFor="typePasswordX">
                         Password
                       </label>
                       <input
@@ -79,9 +82,7 @@ function Login() {
                         </a>
                       </p>
                     </div>                  
-                    <button className="d-block btn btn-outline-light btn-lg " 
-                    style={{margin:"auto"}} 
-                    onClick={handleLogin}>Login</button>
+                    <button className="d-block btn btn-outline-light btn-lg " style={{margin:"auto"}} onClick={handleLogin}>Login</button>
                    
                   </div>
                 </div>
@@ -92,6 +93,6 @@ function Login() {
       </section>
     </>
   );
-}
+  }
 
 export default Login;
